0423d209cf791cf920ee81966c30adb4fd6e517a
Naya Al Tahan <naya.altahan00@gmail.com>
2019-12-17 12:30:59 +0100

    Delete stage tables data after initial load

--- Creating DW Facts and their initial load.sql
+++ Creating DW Facts and their initial load.sql
@@ -167,3 +167,15 @@ select Su_Plant_ID, Su_Profile_ID, Su_Date_ID ,Su_Time_ID, Su_User_ID, Sensor_Va
 from Stage_SEP4_PMI.dbo.Stage_Fact_tem;
 
 select * from Dim_Fact_Tem;
+
+
+-------------------------------------Delete stage data after initial load--------------------------------------------------------------------------
+Delete from Stage_SEP4_PMI.dbo.Stage_Fact_CO2;
+Delete from Stage_SEP4_PMI.dbo.Stage_Fact_Hum;
+Delete from Stage_SEP4_PMI.dbo.Stage_Fact_Light;
+Delete from Stage_SEP4_PMI.dbo.Stage_Fact_Tem;
+Delete from Stage_SEP4_PMI.dbo.stage_dim_PlantProfile;
+Delete from Stage_SEP4_PMI.dbo.stage_dim_Plant;
+Delete from Stage_SEP4_PMI.dbo.stage_dim_Users;
+Delete from Stage_SEP4_PMI.dbo.stage_dim_Time;
+Delete from Stage_SEP4_PMI.dbo.stage_dim_Calendar;
